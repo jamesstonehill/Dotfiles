40b0617186bc35955f748357bfe20c219170c77d
James Stonehill <james.stonehill@alphasights.com>
2017-09-20 14:10:25 +0100

    adding to setup command

--- setup.rb
+++ setup.rb
@@ -3,8 +3,18 @@
 # zsh setup
 puts "Setting up ZSH"
 
-if File.exists?("./.zshrc")
+ZSHRC_DESTINATION_PATH = Pathname.new("../test.txt")
+ZSHRC_SOURCE_PATH = Pathname.new("./zsh/.zshrc")
 
-else
+if ZSH_FILE_PATH.exist?
+  puts "A .zshrc file already exists."
+  puts "Would you like to overwrite it or append to it? (o/A)"
+  response = gets.chomp.downcase
 
+  acceptable_answers = ["o", "a", ""]
+
+  while !acceptable_answers.include?(response)
+    puts "That was not a valid input. Please try again."
+    response = gets.chomp.downcase
+  end
 end
