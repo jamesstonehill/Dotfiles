c41c5e2945235a8181414d097bf161d0fe5daf18
James Stonehill <jamesstonehill@gmail.com>
2026-05-14 23:07:27 +0100

    Add inline error messages

--- lua/language_server_config.lua
+++ lua/language_server_config.lua
@@ -151,12 +151,29 @@ vim.lsp.config('eslint', { capabilities = capabilities })
 
 vim.lsp.enable({ 'pyright', 'ts_ls', 'terraformls', 'eslint' })
 
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = '●',
+    source = 'if_many',
+  },
+  float = { source = true },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+})
+
+-- Show diagnostics float on cursor hold
+vim.api.nvim_create_autocmd('CursorHold', {
+  callback = function()
+    vim.diagnostic.open_float(nil, { focus = false })
+  end,
+})
+vim.opt.updatetime = 500
+
 -- Global mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
---vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float)
---vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
---vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
---vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
